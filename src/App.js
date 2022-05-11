2ea19820a74d88a9d4c44a27fa43c316f5e2a048
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-11 22:40:47 +0400

    create getAdvice function that fetches data from the api and displays it using useState hook, while it is fetching dice button will change to loading animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import React, { useState } from 'react'
+
 import './scss/main.scss'
 import Card from './components/Card'
 
 function App() {
+	const [advice, setAdvice] = useState(
+		'Try buying a coffee for the creator of a free public API, now and then.'
+	)
+	const [adviceNumber, setAdviceNumber] = useState('219')
+	const [loading, setLoading] = useState(false)
+
+	const adviceUrl = 'https://api.adviceslip.com/advice'
+
+	const asyncTimeout = ms => {
+		return new Promise(resolve => {
+			setTimeout(resolve, ms)
+		})
+	}
+
+	const getAdvice = async () => {
+		try {
+			setLoading(true)
+			await asyncTimeout(500)
+			const data = await fetch(adviceUrl)
+			const advice = await data.json()
+			setLoading(false)
+			setAdvice(advice.slip.advice)
+			setAdviceNumber(advice.slip.id)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
 	return (
 		<div className='App'>
 			<main>
-				<Card />
+				<Card
+					advice={advice}
+					adviceNumber={adviceNumber}
+					getAdvice={getAdvice}
+					loading={loading}
+				/>
 			</main>
 		</div>
 	)
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,21 @@
 import React from 'react'
 import Divider from '../images/pattern-divider-mobile.svg'
 import Dice from '../images/icon-dice.svg'
+import Loading from '../images/loading-animation.svg'
 
-const card = () => {
-	let adviceNumber = 13
-
+const Card = ({ advice, adviceNumber, getAdvice, loading }) => {
 	return (
 		<div className='card'>
-			<h4 className='card__adviceNumber'>ADVICE #{adviceNumber}</h4>
-			<h2 className='card__advice'>
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, nemo.
-			</h2>
+			<h4 className='card__adviceNumber'>ADVICE # {adviceNumber}</h4>
+			<h2 className='card__advice'>"{advice}"</h2>
 			<div className='card__divider'>
-				<img src={Divider} alt='fsdfsd' />
+				<img src={Divider} alt='' />
 			</div>
-			<button className='card__diceButton'>
-				<img src={Dice} alt='' />
+			<button onClick={getAdvice} className='card__diceButton'>
+				<img src={loading ? Loading : Dice} alt='' />
 			</button>
 		</div>
 	)
 }
 
-export default card
+export default Card
